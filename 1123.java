b949a1263a555e4903cc382a8498c0143b42f04a
Hyunjun-study <162002859+Hyunjun-study@users.noreply.github.com>
2024-03-07 14:25:44 +0900

    Create 1123.java

---
+++ 1123.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        float b =(float) 9/5*a+32;
+        System.out.printf("%.3f",b);
+    }
+ 
+}
