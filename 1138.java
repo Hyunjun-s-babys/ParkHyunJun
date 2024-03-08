8ae37f874e52967f1cf90b95f20d332e122cf53a
Hyunjun-study <162002859+Hyunjun-study@users.noreply.github.com>
2024-03-08 18:05:18 +0900

    Create 1138.java

---
+++ 1138.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int b =sc.nextInt();
+        int a = b ==1 ? 0 : 1 ;
+        System.out.println(a);
+        
+    }
+
+}
