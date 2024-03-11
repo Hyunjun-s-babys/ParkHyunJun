9ae579904859297ed2d3935916573c80bc4ac41e
Hyunjun-study <162002859+Hyunjun-study@users.noreply.github.com>
2024-03-11 15:58:47 +0900

    Create 1160.java

---
+++ 1160.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a =sc.nextDouble();
+
+        if(a%2!=0){
+            System.out.println("oh my god");
+        }
+    
+        else{
+            System.out.println("enjoy");
+        }
+
+    }
+}
