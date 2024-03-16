0831aa696a781e893c9cf3d158ddd2080fc4d769
Hyunjun-study <162002859+Hyunjun-study@users.noreply.github.com>
2024-03-17 02:10:00 +0900

    Create 1171.java

---
+++ 1171.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String a =sc.next();
+        int b =sc.nextInt();
+        int c =sc.nextInt();
+
+        if(b<10){
+            if(c<10){
+                System.out.println(a+"0"+b+"00"+c);
+            }
+            else if(c<100){
+                System.out.println(a+"0"+b+"0"+c);
+            }
+            else{
+                System.out.println(a+"0"+b+c);
+            }
+        }else{
+            if(c<10){
+                System.out.println(a+b+"00"+c);
+            }
+            else if(c<100){
+                System.out.println(a+b+"0"+c);
+            }
+            else{
+                System.out.println(a+b+c);
+            }
+        }
+
+    }
+
+}
