52f4a1c033f2d7767a8465656c229da00d437077
Hyunjun-study <162002859+Hyunjun-study@users.noreply.github.com>
2024-03-19 09:26:14 +0900

    Create 1224.java

---
+++ 1224.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        double a = sc.nextInt();
+        double b = sc.nextInt();
+        double c = sc.nextInt();
+        double d = sc.nextInt();
+
+
+        if(a/b > c/d){
+            System.out.println(">");
+        }else if(a/b == c/d){
+            System.out.println("=");
+        }else{
+            System.out.println("<");
+        }
+
+    }
+
+}
